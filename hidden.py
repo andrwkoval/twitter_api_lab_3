d871f511afd678e67343848e4c21435127f09b9e
Andrii Koval <drone.kov@gmail.com>
2018-02-18 21:33:31 +0200

    first commit

---
+++ hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "N9w0zK5zsr2OXuNAiyZbxNT7a",
+            "consumer_secret": "HaIufiQBktKC7EgPaFG7hF3lgWiQaz8LW6osmcEmqyYnsgMaBl",
+            "token_key": "3010150012-BMZZPKDetg52eHPWUVv8JyBFz6e1uWOm7hLv8K5",
+            "token_secret": "wWpPtAod7BBV4SnqnUc27PSfcqKkokElkZgMpatD1vBio"}
---
+++ tw_json_loader.py
@@ -0,0 +1,25 @@
+import urllib.request, urllib.parse, urllib.error
+import twurl
+import json
+import ssl
+
+
+def get_js_data(username):
+
+    TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
+
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+
+    url = twurl.augment(TWITTER_URL,
+                        {'screen_name': username, 'count': '5'})
+
+    connection = urllib.request.urlopen(url, context=ctx)
+    data = connection.read().decode()
+    js = json.loads(data, encoding="utf-8")
+
+    return js
+
+
+print(get_js_data("King_Koval"))
